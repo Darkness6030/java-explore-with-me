e09cc754e4e1a83f88f7f91b87afcb2b1e3ec9eb
Darkness <inflexibled@gmail.com>
2024-04-19 21:31:14 +0300

    :skull:

--- stats-service/server/src/main/java/ru/practicum/server/repository/StatsServerRepository.java
+++ stats-service/server/src/main/java/ru/practicum/server/repository/StatsServerRepository.java
@@ -12,14 +12,14 @@ public interface StatsServerRepository extends JpaRepository<EndpointHit, Long> 
 
     @Query("SELECT new ru.practicum.server.model.StatHits(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
             "FROM EndpointHit AS e " +
-            "WHERE e.timestamp BETWEEN ?1 AND ?2 AND (e.uri IN ?3 OR ?3 = null) " +
+            "WHERE e.timestamp BETWEEN ?1 AND ?2 AND (e.uri IN ?3 OR ?3 IS EMPTY) " +
             "GROUP BY e.app, e.uri " +
             "ORDER BY COUNT(DISTINCT e.ip) DESC")
     List<StatHits> findUniqueStatsWithUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
     @Query("SELECT new ru.practicum.server.model.StatHits(e.app, e.uri, COUNT(e.ip)) " +
             "FROM EndpointHit as e " +
-            "WHERE e.timestamp BETWEEN ?1 AND ?2 AND (e.uri IN ?3 OR ?3 = null) " +
+            "WHERE e.timestamp BETWEEN ?1 AND ?2 AND (e.uri IN ?3 OR ?3 IS EMPTY) " +
             "GROUP BY e.app, e.uri " +
             "ORDER BY COUNT(e.ip) DESC")
     List<StatHits> findAllStatsWithUris(LocalDateTime start, LocalDateTime end, List<String> uris);
