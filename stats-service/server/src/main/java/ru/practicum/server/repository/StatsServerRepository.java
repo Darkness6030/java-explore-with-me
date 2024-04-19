2161646b320b5444c97673177eab8feeee957b34
Darkness <inflexibled@gmail.com>
2024-04-19 20:56:04 +0300

    !!!

--- stats-service/server/src/main/java/ru/practicum/server/repository/StatsServerRepository.java
+++ stats-service/server/src/main/java/ru/practicum/server/repository/StatsServerRepository.java
@@ -8,14 +8,14 @@ import java.util.List;
 
 public interface StatsServerRepository extends JpaRepository<EndpointHit, Long> {
 
-    @Query("SELECT new ru.practicum.model.StatHits(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
+    @Query("SELECT new ru.practicum.server.model.StatHits(e.app, e.uri, COUNT(DISTINCT e.ip)) " +
             "FROM EndpointHit AS e " +
             "WHERE e.timestamp BETWEEN ?1 AND ?2 AND (e.uri IN ?3 OR ?3 = null) " +
             "GROUP BY e.app, e.uri " +
             "ORDER BY COUNT(DISTINCT e.ip) DESC")
     List<StatHits> findUniqueStatsWithUris(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.model.StatHits(e.app, e.uri, COUNT(e.ip)) " +
+    @Query("SELECT new ru.practicum.server.model.StatHits(e.app, e.uri, COUNT(e.ip)) " +
             "FROM EndpointHit as e " +
             "WHERE e.timestamp BETWEEN ?1 AND ?2 AND (e.uri IN ?3 OR ?3 = null) " +
             "GROUP BY e.app, e.uri " +
