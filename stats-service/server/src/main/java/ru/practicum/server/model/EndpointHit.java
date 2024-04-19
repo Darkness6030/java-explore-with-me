19ed383ca42ecb4916de03f6be1297b165d07672
Darkness <inflexibled@gmail.com>
2024-04-19 21:24:57 +0300

    :skull:

--- stats-service/server/src/main/java/ru/practicum/server/model/EndpointHit.java
+++ stats-service/server/src/main/java/ru/practicum/server/model/EndpointHit.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "hits")
+@Table(name = "endpoint_hits")
 @Data
 @Builder
 public class EndpointHit {
