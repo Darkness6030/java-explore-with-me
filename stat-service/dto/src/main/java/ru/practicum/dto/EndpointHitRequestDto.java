bc414ef61b7a68ab8afd5bb4395db93a9b08f9cf
Darkness <inflexibled@gmail.com>
2024-04-23 17:48:01 +0300

    test

--- stat-service/dto/src/main/java/ru/practicum/dto/EndpointHitRequestDto.java
+++ stat-service/dto/src/main/java/ru/practicum/dto/EndpointHitRequestDto.java
@@ -2,24 +2,25 @@ package ru.practicum.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class EndpointHitRequestDto {
     @NotBlank
     private String app;
+
     @NotBlank
     private String uri;
+
     @NotBlank
     private String ip;
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
